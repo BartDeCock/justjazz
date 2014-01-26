cb54fbe368a490febaa732ee211ec7cad6d8abbc
Bart DC <mrdee72@gmail.com>
2014-01-26 15:05:04 +0100

    test

--- justjazz.be/index.php
+++ justjazz.be/index.php
@@ -8,7 +8,7 @@
 	
 		<img id="image_home_right" src="MG_7432.jpg"/>
 	
-	<a href="victory.php"><span id="special_word">The Victory Jazzband</span></a> en <a href="justjazz.php"><span id="special_word">Just Jazz</span></a>, </br>
+	<a href="victory.php"><span id="special_word">TEST TEST</span></a> en <a href="justjazz.php"><span id="special_word">Just Jazz</span></a>, </br>
 
 brengen de live-music die ongetwijfeld een meerwaarde zal geven aan al wat u organiseert.
 
